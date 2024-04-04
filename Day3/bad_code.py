e3831267435cd21bcf15c94c765c1e71bb86be1f
lhaacke <122768730+lhaacke@users.noreply.github.com>
2024-04-04 19:11:21 +1100

    added code for improving exercise

---
+++ Day3/bad_code.py
@@ -0,0 +1,15 @@
+import numpy as np
+import random
+
+random.seed(42)
+options = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+groups = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+all = [{'rank': group, 'suit': option} for group in groups for option in options]
+random.shuffle(all)
+handout = {f'{i+1}': [] for i in range(4)}
+for _ in range(5):
+    for p in handout:
+        a = all.pop()
+        handout[p].append(a)
+for p, h in handout.items():
+    print(f"{p}'s hand: {h}") 
