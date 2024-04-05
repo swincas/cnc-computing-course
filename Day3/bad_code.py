53c4b01a5f9cb1565767200c9633f9ee954df747
lhaacke <lydia.haacke@gmail.com>
2024-04-05 11:16:37 +1100

    test_change_for_forking

--- Day3/bad_code.py
+++ Day3/bad_code.py
@@ -1,10 +1,10 @@
 import numpy as np
 import random
 
-random.seed(32)
-options = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-groups = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-all = [{'rank': group, 'suit': option} for group in groups for option in options]
+random.seed(42)
+rank = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+all = [{'rank': rank, 'suit': option} for group in groups for option in options]
 random.shuffle(all)
 handout = {f'{i+1}': [] for i in range(4)}
 for _ in range(5):
