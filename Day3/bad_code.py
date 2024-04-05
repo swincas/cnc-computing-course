e9eebef837a5f148cd84632fa78e6cb31cca45e0
Ovive57 <ovive.pro@gmail.com>
2024-04-05 11:13:43 +1100

    Added comment

--- Day3/bad_code.py
+++ Day3/bad_code.py
@@ -1,11 +1,11 @@
 import numpy as np
 import random
 
-random.seed(32)
+random.seed(52)
 options = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 groups = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
 all = [{'rank': group, 'suit': option} for group in groups for option in options]
-random.shuffle(all)
+random.shuffle(all) # shuffle cards
 handout = {f'{i+1}': [] for i in range(4)}
 for _ in range(5):
     for p in handout:
