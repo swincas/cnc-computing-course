76fff9c2db8db7d886d45483e5ebb8d42dfe4057
Tyson Dial <157274509+tdial2000@users.noreply.github.com>
2024-10-03 15:20:45 +1000

    Create run_python_code.sh

---
+++ Day1/run_python_code.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# load python modules
+
+
+
+# run python code to add numbers together
+python3 add_numbers.py
+
+
+
+# run python code to get mean of numbers
+# python3 get_mean.py
