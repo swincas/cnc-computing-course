2c67e07c894df9200dc61f20800e227393674229
Tyson Dial <157274509+tdial2000@users.noreply.github.com>
2024-10-03 15:18:47 +1000

    Create add_numbers.py

---
+++ Day1/add_numbers.py
@@ -0,0 +1,15 @@
+# imports
+import numpy as np
+
+
+# create an array of numbers up to 100
+numbers = np.arange(100)
+
+# add numbers together
+sum = np.sum(numbers)
+
+# print result
+print(f"Sum of numbers up to 100: {sum}")
+
+
+
