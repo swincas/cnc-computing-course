c86c8c80ac8efb80f87982bbaf506782776c2b37
Tyson Dial <157274509+tdial2000@users.noreply.github.com>
2024-10-03 15:22:18 +1000

    Create get_mean.py

---
+++ Day1/python_code/get_mean.py
@@ -0,0 +1,13 @@
+# imports
+import numpy as np
+
+
+# get numbers up to 100
+numbers = np.arange(100)
+
+# get mean
+mean = np.mean(numbers)
+
+# print result
+print(f"Mean of numbers up to 100: {mean}")
+
