8bfe3aaa374bf4c84af2b583d586bb2746f71b73
sparrowroch <60152365+sparrowroch@users.noreply.github.com>
2024-10-11 12:20:40 +1100

    Changes to bad_code.py

--- Day3/bad_code.py
+++ Day3/bad_code.py
@@ -1,16 +1,25 @@
 import numpy as np
 import random
 
-# hi
+# Deals 5 cards from a deck to four players.
+# Originally, the code printed stuff like "{'rank': 'Diamonds', 'suit': '4'}"
+# so the rank and suit labels have been switched around.
 random.seed(52)
-options = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-groups = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-all = [{'rank': group, 'suit': option} for group in groups for option in options]
-random.shuffle(all) # shuffle cards
-handout = {f'{i+1}': [] for i in range(4)}
+ranks = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+deck = [{'rank': rank, 'suit': suit} for suit in suits for rank in ranks]
+
+# Shuffles cards.
+random.shuffle(deck)
+
+# Deals cards to players.
+deal = {f'{i+1}': [] for i in range(4)}
 for _ in range(5):
-    for p in handout:
-        a = all.pop()
-        handout[p].append(a)
-for p, h in handout.items():
-    print(f"{p}'s hand: {h}") 
+    for player in deal:
+        a = deck.pop()
+        deal[player].append(a)
+
+# Prints the cards in each player's hand.
+for player, hand in deal.items():
+    results = ', '.join([f"{card['rank']} of {card['suit']}" for card in hand])
+    print(f"{player}'s hand: {results}")
