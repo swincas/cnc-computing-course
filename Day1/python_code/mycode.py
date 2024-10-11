9312fe8fcb750e68fd9a1fe819152bf94d40f52c
lhaacke <lydia.haacke@gmail.com>
2024-10-11 11:23:09 +1100

    added print_goodbye function to mycode.py

--- Day1/python_code/mycode.py
+++ Day1/python_code/mycode.py
@@ -1,3 +1,6 @@
 def print_hello():
   print("Hello World")
 
+def print_goodbye():
+  print("Goodbye World")
+
--- Day1/python_code/say_hello.py
+++ Day1/python_code/say_hello.py
@@ -1,4 +1,5 @@
 # imports
-from mycode import print_hello
+from mycode import print_hello, print_goodbye
 
 print_hello()
+print_goodbye()
