6e514803ceb352b6b4b62e9770fe41e961009fd9
lhaacke <lydia.haacke@gmail.com>
2024-10-11 11:15:14 +1100

    changed size of array in estimate_resources.py

--- Day1/python_code/estimate_resources.py
+++ Day1/python_code/estimate_resources.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # estimate memory usage of this array
 # estimate how long it takes to create
-big_arr_1 = np.ones((100000,100000))
+big_arr_1 = np.ones((10000,10000))
 big_arr_2 = np.full_like(big_arr_1, 2)
 
 sum = np.sum(big_arr_1) + np.sum(big_arr_2)
